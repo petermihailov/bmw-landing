c8e4d837134371f643314565d5708d821ade3910
Peter Mihailov <usernameispeter@gmail.com>
2019-01-13 14:32:32 +0300

    gallery slide show

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,189 +1,211 @@
-/*
-* PhotoSwipe
-* */
-const initPhotoSwipeFromDOM = function (gallerySelector) {
-  const parseThumbnailElements = (el) => {
-    const thumbElements = el.childNodes;
-    const numNodes = thumbElements.length;
-    const items = [];
+(() => {
+  /*
+  * PhotoSwipe
+  * */
+  const initPhotoSwipeFromDOM = function (gallerySelector) {
+    const parseThumbnailElements = (el) => {
+      const thumbElements = el.childNodes;
+      const numNodes = thumbElements.length;
+      const items = [];
 
-    for (let i = 0; i < numNodes; i++) {
-      const figureEl = thumbElements[i];
+      for (let i = 0; i < numNodes; i++) {
+        const figureEl = thumbElements[i];
 
-      if (figureEl.nodeType !== 1) {
-        continue;
+        if (figureEl.nodeType !== 1) {
+          continue;
+        }
+
+        const linkEl = figureEl.children[0];
+        const size = linkEl.getAttribute('data-size').split('x');
+
+        const item = {
+          src: linkEl.getAttribute('href'),
+          w: parseInt(size[0], 10),
+          h: parseInt(size[1], 10),
+        };
+
+        if (figureEl.children.length > 1) {
+          item.title = figureEl.children[1].innerHTML;
+        }
+
+        if (linkEl.children.length > 0) {
+          item.msrc = linkEl.children[0].getAttribute('src');
+        }
+
+        item.el = figureEl;
+        items.push(item);
       }
 
-      const linkEl = figureEl.children[0];
-      const size = linkEl.getAttribute('data-size').split('x');
-
-      const item = {
-        src: linkEl.getAttribute('href'),
-        w: parseInt(size[0], 10),
-        h: parseInt(size[1], 10),
-      };
-
-      if (figureEl.children.length > 1) {
-        item.title = figureEl.children[1].innerHTML;
-      }
-
-      if (linkEl.children.length > 0) {
-        item.msrc = linkEl.children[0].getAttribute('src');
-      }
-
-      item.el = figureEl;
-      items.push(item);
-    }
-
-    return items;
-  };
-
-  const closest = function closest(el, fn) {
-    return el && (fn(el) ? el : closest(el.parentNode, fn));
-  };
-
-  const onThumbnailsClick = (e) => {
-    e = e || window.event;
-    e.preventDefault ? e.preventDefault() : e.returnValue = false;
-
-    const eTarget = e.target || e.srcElement;
-
-    const clickedListItem = closest(eTarget, function (el) {
-      return (el.tagName && el.tagName.toUpperCase() === 'FIGURE');
-    });
-
-    if (!clickedListItem) {
-      return;
-    }
-
-    const clickedGallery = clickedListItem.parentNode;
-    const childNodes = clickedListItem.parentNode.childNodes;
-    const numChildNodes = childNodes.length;
-    let nodeIndex = 0, index;
-
-    for (let i = 0; i < numChildNodes; i++) {
-      if (childNodes[i].nodeType !== 1) {
-        continue;
-      }
-
-      if (childNodes[i] === clickedListItem) {
-        index = nodeIndex;
-        break;
-      }
-
-      nodeIndex++;
-    }
-
-
-    if (index >= 0) {
-      openPhotoSwipe(index, clickedGallery);
-    }
-
-    return false;
-  };
-
-  const photoswipeParseHash = () => {
-    const hash = window.location.hash.substring(1);
-    const params = {};
-
-    if (hash.length < 5) {
-      return params;
-    }
-
-    const vars = hash.split('&');
-
-    for (let i = 0; i < vars.length; i++) {
-      if (!vars[i]) {
-        continue;
-      }
-
-      const pair = vars[i].split('=');
-
-      if (pair.length < 2) {
-        continue;
-      }
-
-      params[pair[0]] = pair[1];
-    }
-
-    if (params.gid) {
-      params.gid = parseInt(params.gid, 10);
-    }
-
-    return params;
-  };
-
-  const openPhotoSwipe = function (index, galleryElement, disableAnimation, fromURL) {
-    const pswpElement = document.querySelectorAll('.pswp')[0];
-    const items = parseThumbnailElements(galleryElement);
-    const options = {
-      galleryUID: galleryElement.getAttribute('data-pswp-uid'),
-      getThumbBoundsFn: (index) => {
-        const thumbnail = items[index].el.getElementsByTagName('img')[0]; // find thumbnail
-        const pageYScroll = window.pageYOffset || document.documentElement.scrollTop;
-        const rect = thumbnail.getBoundingClientRect();
-
-        return {x: rect.left, y: rect.top + pageYScroll, w: rect.width};
-      },
+      return items;
     };
 
-    if (fromURL) {
-      if (options.galleryPIDs) {
-        for (let j = 0; j < items.length; j++) {
-          if (items[j].pid == index) {
-            options.index = j;
-            break;
+    const closest = function closest(el, fn) {
+      return el && (fn(el) ? el : closest(el.parentNode, fn));
+    };
+
+    const onThumbnailsClick = (e) => {
+      e = e || window.event;
+      e.preventDefault ? e.preventDefault() : e.returnValue = false;
+
+      const eTarget = e.target || e.srcElement;
+
+      const clickedListItem = closest(eTarget, function (el) {
+        return (el.tagName && el.tagName.toUpperCase() === 'FIGURE');
+      });
+
+      if (!clickedListItem) {
+        return;
+      }
+
+      const clickedGallery = clickedListItem.parentNode;
+      const childNodes = clickedListItem.parentNode.childNodes;
+      const numChildNodes = childNodes.length;
+      let nodeIndex = 0, index;
+
+      for (let i = 0; i < numChildNodes; i++) {
+        if (childNodes[i].nodeType !== 1) {
+          continue;
+        }
+
+        if (childNodes[i] === clickedListItem) {
+          index = nodeIndex;
+          break;
+        }
+
+        nodeIndex++;
+      }
+
+
+      if (index >= 0) {
+        openPhotoSwipe(index, clickedGallery);
+      }
+
+      return false;
+    };
+
+    const photoswipeParseHash = () => {
+      const hash = window.location.hash.substring(1);
+      const params = {};
+
+      if (hash.length < 5) {
+        return params;
+      }
+
+      const vars = hash.split('&');
+
+      for (let i = 0; i < vars.length; i++) {
+        if (!vars[i]) {
+          continue;
+        }
+
+        const pair = vars[i].split('=');
+
+        if (pair.length < 2) {
+          continue;
+        }
+
+        params[pair[0]] = pair[1];
+      }
+
+      if (params.gid) {
+        params.gid = parseInt(params.gid, 10);
+      }
+
+      return params;
+    };
+
+    const openPhotoSwipe = function (index, galleryElement, disableAnimation, fromURL) {
+      const pswpElement = document.querySelectorAll('.pswp')[0];
+      const items = parseThumbnailElements(galleryElement);
+      const options = {
+        galleryUID: galleryElement.getAttribute('data-pswp-uid'),
+        getThumbBoundsFn: (index) => {
+          const thumbnail = items[index].el.getElementsByTagName('img')[0]; // find thumbnail
+          const pageYScroll = window.pageYOffset || document.documentElement.scrollTop;
+          const rect = thumbnail.getBoundingClientRect();
+
+          return {x: rect.left, y: rect.top + pageYScroll, w: rect.width};
+        },
+      };
+
+      if (fromURL) {
+        if (options.galleryPIDs) {
+          for (let j = 0; j < items.length; j++) {
+            if (items[j].pid == index) {
+              options.index = j;
+              break;
+            }
           }
+        } else {
+          options.index = parseInt(index, 10) - 1;
         }
       } else {
-        options.index = parseInt(index, 10) - 1;
+        options.index = parseInt(index, 10);
       }
-    } else {
-      options.index = parseInt(index, 10);
+
+      if (isNaN(options.index)) {
+        return;
+      }
+
+      if (disableAnimation) {
+        options.showAnimationDuration = 0;
+      }
+
+      const gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
+      gallery.init();
+    };
+
+    const galleryElements = document.querySelectorAll(gallerySelector);
+    for (let i = 0, l = galleryElements.length; i < l; i++) {
+      galleryElements[i].setAttribute('data-pswp-uid', i + 1);
+      galleryElements[i].onclick = onThumbnailsClick;
     }
 
-    if (isNaN(options.index)) {
-      return;
+    const hashData = photoswipeParseHash();
+    if (hashData.pid && hashData.gid) {
+      openPhotoSwipe(hashData.pid, galleryElements[hashData.gid - 1], true, true);
     }
-
-    if (disableAnimation) {
-      options.showAnimationDuration = 0;
-    }
-
-    const gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
-    gallery.init();
   };
 
-  const galleryElements = document.querySelectorAll(gallerySelector);
-  for (let i = 0, l = galleryElements.length; i < l; i++) {
-    galleryElements[i].setAttribute('data-pswp-uid', i + 1);
-    galleryElements[i].onclick = onThumbnailsClick;
-  }
-
-  const hashData = photoswipeParseHash();
-  if (hashData.pid && hashData.gid) {
-    openPhotoSwipe(hashData.pid, galleryElements[hashData.gid - 1], true, true);
-  }
-};
-
 // init
-initPhotoSwipeFromDOM('.gallery');
+  initPhotoSwipeFromDOM('.gallery');
 
+  /*
+  * GliderJS
+  * */
+  window.addEventListener('load', function () {
+    const delay = 3000;
+    const elements = document.querySelectorAll('.glider');
 
-/*
-* GliderJS
-* */
-window.addEventListener('load', function () {
-  const elements = document.querySelectorAll('.glider');
+    elements.forEach((el) => {
+      const idx = el.dataset.index;
+      const prev = `[data-index="${idx}"] ~ .glider-prev`;
+      const next = `[data-index="${idx}"] ~ .glider-next`;
+      const startSlideShow = () => interval = setInterval(() => glider.scrollItem('next'), delay);
+      let interval = null;
 
-  elements.forEach((el) => {
-    const idx = el.dataset.index;
+      const glider = new Glider(el, {
+        rewind: true,
+        arrows: {
+          prev,
+          next,
+        },
+      });
 
-    new Glider(el, {
-      arrows: {
-        prev: `[data-index="${idx}"] ~ .glider-prev`,
-        next: `[data-index="${idx}"] ~ .glider-next`,
-      },
-    })
-  })
-});
+      startSlideShow();
+
+      window.addEventListener('click', (e) => {
+        const isClickInside = el.parentElement.contains(e.target);
+
+        if (isClickInside) {
+          clearInterval(interval);
+          interval = null;
+        } else {
+          if (!interval) {
+            startSlideShow();
+          }
+        }
+      });
+    });
+  });
+})();
